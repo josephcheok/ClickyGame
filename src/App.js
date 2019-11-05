2951996c8cf5101034133f801c5d301a9e6b642c
josephcheok <joseph@cheokandco.com>
2019-11-06 02:11:55 +1100

    Grid setup with pictures and randomisation on click done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ClickyGame from "./components/ClickyGame";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClickyGame />
+      </header>
+    </div>
+  );
+}
+
+export default App;
